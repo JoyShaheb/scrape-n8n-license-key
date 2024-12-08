8602567a5613197f4d686fd34ba78335b187de94
mitka <mitka_j@yahoo.com>
2024-12-08 15:58:27 +0600

    learning wip

--- learn.js
+++ learn.js
@@ -1,4 +1,9 @@
 import NodeRSA from "node-rsa";
+import crypto from "crypto-js";
+import nodeMachineId from "node-machine-id";
+import nodeRSA from "node-rsa";
+// import undici from "undici";
+
 const key = new NodeRSA({ b: 512 });
 const publicKey = key.exportKey("public");
 const privateKey = key.exportKey("private");
@@ -7,4 +12,4 @@ const text = "Hello RSA!";
 const encrypted = key.encrypt(text, "base64");
 console.log("encrypted: ", encrypted);
 const decrypted = key.decrypt(encrypted, "utf8");
-console.log("decrypted: ", decrypted);
+console.log("decrypted:: ", decrypted);
